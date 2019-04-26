655af2c84126c21cbaf3a4e749827e11d8f2c7d4
unknown <bubbastinx@gmail.com>
2019-04-26 09:55:02 -0500

    added parcel broke out components

--- App.js
+++ App.js
@@ -1,35 +1,18 @@
-const Pet = props => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, props.name),
-    React.createElement('h1', {}, props.animal),
-    React.createElement('h1', {}, props.breed)
-  ]);
-};
+import React from 'react';
+import { render } from 'react-dom';
+import Pet from './Pet';
 
 class App extends React.Component {
-  handleTitleClick() {
-    alert('You clicked the Title');
-  }
   render() {
-    return React.createElement('div', {}, [
-      React.createElement('h1', { onClick: this.handleTitleClick }, 'Adopt Me'),
-      React.createElement(Pet, {
-        name: 'Lunda',
-        animal: 'Dog',
-        breed: 'Whoknows'
-      }),
-      React.createElement(Pet, {
-        name: 'Lunda',
-        animal: 'Dog',
-        breed: 'Whoknows'
-      }),
-      React.createElement(Pet, {
-        name: 'Lunda',
-        animal: 'Dog',
-        breed: 'Whoknows'
-      })
-    ]);
+    return (
+      <div>
+        <h1>Adopt Me</h1>
+        <Pet name="Lunda" animal="Dog" breed="Whoknows" />
+        <Pet name="Sam" animal="Log" breed="Whoknows" />
+        <Pet name="Tham" animal="Fog" breed="Whoknows" />
+      </div>
+    );
   }
 }
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+render(<App />, document.getElementById('root'));
