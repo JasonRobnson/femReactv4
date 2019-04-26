bb78dee3d576bb8b277518b29846a434ab6c1050
unknown <bubbastinx@gmail.com>
2019-04-26 10:43:05 -0500

    connected to petfinder api

--- App.js
+++ App.js
@@ -1,8 +1,39 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Pet from './Pet';
+import pf from 'petfinder-client';
 
+const petfinder = pf();
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pets: []
+    };
+  }
+  componentDidMount() {
+    petfinder.pet
+      .find({ output: 'full', location: 'Minneapolis, MN' })
+      .then(data => {
+        let pets;
+
+        // data is shared in an XML format, and if its a single item returns an object, more than one returns an array
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          pets = [];
+        }
+        this.setState({
+          pets
+        });
+      });
+    // const promise = petfinder.breed.list({ animal: 'dog' });
+    // promise.then(console.log, console.error);
+  }
   render() {
     return (
       <div>
