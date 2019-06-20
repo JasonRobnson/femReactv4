def46ec04ccc177cd34c4e1625c051724f42662d
unknown <bubbastinx@gmail.com>
2019-06-20 12:58:51 -0500

    added reach links

--- App.js
+++ App.js
@@ -1,67 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Pet from './Pet';
-import pf from 'petfinder-client';
+import { Router, Link } from '@reach/router';
 
-const petfinder = pf();
+import Results from './src/Results';
+import Details from './src/Details';
+
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      pets: []
-    };
-  }
-  componentDidMount() {
-    petfinder.pet
-      .find({ output: 'full', location: 'Minneapolis, MN' })
-      .then(data => {
-        let pets;
-        console.log(data);
-        // data is shared in an XML format, and if its a single item returns an object, more than one returns an array
-        if (data.petfinder.pets && data.petfinder.pets.pet) {
-          if (Array.isArray(data.petfinder.pets.pet)) {
-            pets = data.petfinder.pets.pet;
-          } else {
-            pets = [data.petfinder.pets.pet];
-          }
-        } else {
-          pets = [];
-        }
-        this.setState({
-          pets
-        });
-      });
-
-    // const promise = petfinder.breed.list({ animal: 'dog' });
-    // promise.then(console.log, console.error);
-  }
   render() {
     return (
       <div>
-        <h1>Adopt Me</h1>
-        <div>
-          {this.state.pets.map(pet => {
-            let breed;
-            if (Array.isArray(pet.breeds.breed)) {
-              breed = pet.breeds.breed.join(', ');
-            } else {
-              breed = pet.breeds.breed;
-            }
-            return (
-              <Pet
-                key={pet.id}
-                name={pet.name}
-                animal={pet.animal}
-                breed={breed}
-                media={pet.media}
-                location={`${pet.contact.city},${pet.contact.state}`}
-              />
-            );
-          })}
-        </div>
+        <Link to="/">
+          <header>Adopt Me!</header>
+        </Link>
+
+        <Router>
+          <Results path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     );
   }
 }
-
 render(<App />, document.getElementById('root'));
---
+++ src/Results.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import Pet from './Pet';
+import pf from 'petfinder-client';
+
+const petfinder = pf();
+
+class Results extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pets: []
+    };
+  }
+  componentDidMount() {
+    petfinder.pet
+      .find({ output: 'full', location: 'Minneapolis, MN' })
+      .then(data => {
+        let pets;
+        console.log(data);
+        // data is shared in an XML format, and if its a single item returns an object, more than one returns an array
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          pets = [];
+        }
+        this.setState({
+          pets
+        });
+      });
+  }
+  render() {
+    return (
+      <div className="search">
+        {this.state.pets.map(pet => {
+          let breed;
+          if (Array.isArray(pet.breeds.breed)) {
+            breed = pet.breeds.breed.join(', ');
+          } else {
+            breed = pet.breeds.breed;
+          }
+          return (
+            <Pet
+              key={pet.id}
+              name={pet.name}
+              animal={pet.animal}
+              breed={breed}
+              media={pet.media}
+              location={`${pet.contact.city},${pet.contact.state}`}
+              id={pet.id}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+}
+export default Results;
